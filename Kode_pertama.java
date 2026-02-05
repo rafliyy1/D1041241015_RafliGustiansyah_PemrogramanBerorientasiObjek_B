47cc7eadaf6859467a85c1d6176f95138d11ede7
Rafli Gustiansyah <rafligustiansyah01@gmail.com>
2026-02-05 15:50:13 +0700

    Pertemuan1

---
+++ Kode_pertama.java
@@ -0,0 +1,6 @@
+public class Kode_pertama {
+    public static void main(String[] args) {
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah peogram Java pertama saya!");
+    }
+}
