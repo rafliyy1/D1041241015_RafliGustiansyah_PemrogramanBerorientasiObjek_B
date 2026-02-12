779f63bbc96cd36e157a794f51907fbc08a2badf
Rafli Gustiansyah <rafligustiansyah01@gmail.com>
2026-02-12 15:45:48 +0700

    Delete Kode_pertama.java

--- Kode_pertama.java
+++
@@ -1,6 +0,0 @@
-public class Kode_pertama {
-    public static void main(String[] args) {
-        System.out.println("Halo, Pemrograman Berorientasi Objek!");
-        System.out.println("Ini adalah peogram Java pertama saya!");
-    }
-}
