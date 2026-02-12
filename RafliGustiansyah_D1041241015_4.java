95878e2ecd94afd790d01ec944332524b311e31b
Rafli Gustiansyah <rafligustiansyah01@gmail.com>
2026-02-12 15:46:24 +0700

    12 Februari 2026

--- RafliGustiansyah_D1041241015_4.java
+++
@@ -1,8 +0,0 @@
-public class RafliGustiansyah_D1041241015_4 {
-    public static void main(String[] args){
-        System.out.println("Nama        : RafliGustiansyah");
-        System.out.println("NIM         : D1041241015");
-        System.out.println("Semester    : 4");
-        System.out.println("Prodi       : Informatika");
-    }
-}
